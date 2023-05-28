94bb6df99141fcc7910cada1c5c6a1059855b4be
Alejandro Ravasio <alejandroravasio@gmail.com>
2023-05-28 03:11:14 -0300

    Initial Commit

---
+++ slugrules/slugrulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  slugrulesApp.swift
+//  slugrules
+//
+//  Created by Alejandro Ravasio on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct slugrulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
